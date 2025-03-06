848c273b1c2df039406b0d463721dab96fdd2d3c
NadilGT <rccnadildinsara@gmail.com>
2025-03-06 20:25:03 +0530

    remove default code and implement a surface

--- app/src/main/java/com/example/mobilecw/MainActivity.kt
+++ app/src/main/java/com/example/mobilecw/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mobilecw.ui.theme.MobileCWTheme
 
@@ -19,29 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileCWTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ){
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileCWTheme {
-        Greeting("Android")
     }
 }
