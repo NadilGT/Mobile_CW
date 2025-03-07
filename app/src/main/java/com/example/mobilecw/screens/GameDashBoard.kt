902b101765ac14fbc2aca9e6cb4d29a292cb9cfe
NadilGT <rccnadildinsara@gmail.com>
2025-03-07 11:06:30 +0530

    Implement the score with simple logic (Not the real logic)

--- app/src/main/java/com/example/mobilecw/screens/GameDashBoard.kt
+++ app/src/main/java/com/example/mobilecw/screens/GameDashBoard.kt
@@ -22,27 +22,37 @@ import com.example.mobilecw.viewModel.GameViewModel
 @Composable
 fun GameDashBoard(viewModel: GameViewModel = viewModel()){
     val humanDice by viewModel.humanDice.collectAsState()
+    val humanScore by viewModel.humanScore.collectAsState()
     val computerDice by viewModel.computerDice.collectAsState()
+    val computerScore by viewModel.computerScore.collectAsState()
+
+    val rollCount by viewModel.rollCount.collectAsState()
     
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Spacer(modifier = Modifier.height(24.dp))
-        Text(text = "Human Player")
+        Text(text = "Human Player: $humanScore")
         DiceRow(diceValues = humanDice)
         
         Spacer(modifier = Modifier.height(32.dp))
-        Text(text = "Computer")
+        Text(text = "Computer: $computerScore")
         DiceRow(diceValues = computerDice)
 
         Spacer(modifier = Modifier.height(32.dp))
         Row {
-            Button(onClick = {viewModel.rollDices()}) {
+            Button(
+                onClick = {viewModel.rollDices()},
+                enabled = rollCount < 1
+                ) {
                 Text(text = "Throw")
             }
             Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(
+                onClick = {viewModel.updateScore()},
+                enabled = rollCount < 2
+            ) {
                 Text(text = "Score")
             }
         }
--- app/src/main/java/com/example/mobilecw/viewModel/GameViewModel.kt
+++ app/src/main/java/com/example/mobilecw/viewModel/GameViewModel.kt
@@ -9,11 +9,27 @@ class GameViewModel: ViewModel() {
     private val _humanDice = MutableStateFlow(List(5) { Random.nextInt(1,7) })
     val humanDice: StateFlow<List<Int>> = _humanDice
 
+    private val _humanScore = MutableStateFlow(0)
+    val humanScore: StateFlow<Int> = _humanScore
+
     private val _computerDice = MutableStateFlow(List(5) { Random.nextInt(1,7)})
     val computerDice: StateFlow<List<Int>> = _computerDice
+
+    private val _computerScore = MutableStateFlow(0)
+    val computerScore: StateFlow<Int> = _computerScore
+
+    private val _rollCount = MutableStateFlow(0)
+    val rollCount: StateFlow<Int> = _rollCount
 
     fun rollDices(){
         _humanDice.value = List(5) {Random.nextInt(1,7)}
         _computerDice.value = List(5) {Random.nextInt(1,7)}
+        _rollCount.value += 1
+    }
+
+    fun updateScore(){
+        _humanScore.value += _humanDice.value.sum()
+        _computerScore.value += _computerDice.value.sum()
+        _rollCount.value = 0
     }
 }
